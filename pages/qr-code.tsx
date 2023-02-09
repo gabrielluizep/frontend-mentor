9af50e2cb52703497e7caed61c8ab1c3ee861508
gabrielluizep <gabrielluizep@hotmail.com>
2023-02-09 15:48:53 -0300

    :sparkles: finished first challenge

---
+++ pages/qr-code.tsx
@@ -0,0 +1,9 @@
+import QrCodeCard from '@/components/QrCodeCard';
+
+export default function QrCode() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-100">
+      <QrCodeCard />
+    </div>
+  );
+}
